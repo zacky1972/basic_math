e68be0f2612af3223e1910902d331fa2fb5f28a6
Susumu Yamazaki <zacky1972@gmail.com>
2021-12-07 22:20:11 +0900

    create exponential_nix and init

--- c_src/basic_math/exponential/libexp.c
+++ c_src/basic_math/exponential/libexp.c
@@ -1,1 +1,13 @@
 #include <erl_nif.h>
+
+static ERL_NIF_TERM init(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    return enif_make_atom(env, "ok");
+}
+
+static ErlNifFunc nif_funcs[] =
+{
+    {"init", 0, init}
+};
+
+ERL_NIF_INIT(Elixir.BasicMath.Exponential.ExponentialNif,nif_funcs,NULL,NULL,NULL,NULL)
---
+++ lib/basic_math/exponential/exponential_nif.ex
@@ -0,0 +1,22 @@
+defmodule BasicMath.Exponential.ExponentialNif do
+  require Logger
+
+  @on_load :load_nif
+
+  @doc false
+  @spec load_nif() :: :ok
+  def load_nif do
+    nif_file = '#{Application.app_dir(:basic_math, "priv/libexp")}'
+
+    case :erlang.load_nif(nif_file, 0) do
+      :ok -> :ok
+      {:error, {:reload, _}} -> :ok
+      {:error, reason} -> Logger.error("Failed to load NIF: #{inspect(reason)}")
+    end
+  end
+
+  @spec init() :: :ok
+  def init() do
+    :erlang.nif_error(:nif_not_loaded)
+  end
+end
