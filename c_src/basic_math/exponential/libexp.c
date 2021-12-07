03481e63c1ee105e02c09f9bb83fe725bccf6746
Susumu Yamazaki <zacky1972@gmail.com>
2021-12-07 22:29:51 +0900

    create load, upgrade and unload

--- c_src/basic_math/exponential/libexp.c
+++ c_src/basic_math/exponential/libexp.c
@@ -1,5 +1,19 @@
 #include <erl_nif.h>
 
+int load(ErlNifEnv* caller_env, void** priv_data, ERL_NIF_TERM load_info)
+{
+    return 0;
+}
+
+int upgrade(ErlNifEnv* caller_env, void** priv_data, void** old_priv_data, ERL_NIF_TERM load_info)
+{
+    return 0;
+}
+
+void unload(ErlNifEnv* caller_env, void* priv_data)
+{
+}
+
 static ERL_NIF_TERM init(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     return enif_make_atom(env, "ok");
@@ -10,4 +24,4 @@ static ErlNifFunc nif_funcs[] =
     {"init", 0, init}
 };
 
-ERL_NIF_INIT(Elixir.BasicMath.Exponential.ExponentialNif,nif_funcs,NULL,NULL,NULL,NULL)
+ERL_NIF_INIT(Elixir.BasicMath.Exponential.ExponentialNif, nif_funcs, load, NULL, upgrade, unload)
